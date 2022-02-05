0d9953768850936c4d84ec9d470b6a19fa57f769
sourabhpoddar404 <56801226+sourabhpoddar404@users.noreply.github.com>
2022-02-05 21:41:47 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ config = {
             'timeout': 120
         },
         'dbpedia': {
-             'endpoint': 'http://dbpedia.org/sparql',
+             'endpoint': 'http://porque.cs.upb.de:8890/sparql',
           #  'endpoint': 'http://sda01dbpedia:softrock@131.220.9.219/sparql',
             'one_hop_bloom_file': './data/blooms/spo1.bloom',
             'two_hop_bloom_file': './data/blooms/spo2.bloom'
